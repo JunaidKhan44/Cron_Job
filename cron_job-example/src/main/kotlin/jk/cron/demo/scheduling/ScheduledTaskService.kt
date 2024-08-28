dc94a41d8141c82833f7d1f3c049dbd05a5ca0ca
Junaid-Khan <80524662+JunaidKhan44@users.noreply.github.com>
2024-08-28 14:42:38 +0500

    Update ScheduledTaskService.kt

--- cron_job-example/src/main/kotlin/jk/cron/demo/scheduling/ScheduledTaskService.kt
+++ cron_job-example/src/main/kotlin/jk/cron/demo/scheduling/ScheduledTaskService.kt
@@ -4,6 +4,10 @@ import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 
+/**
+ * @author Junaid Khan
+ *
+ */
 
 @Service
 class ScheduledTaskService {
